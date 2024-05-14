5d501e4672320ae43d414f1d1f2e87aa513504db
Edy Espinal <edyespinal@gmail.com>
2024-05-14 11:40:42 +0200

    feat: add new deep links for improved reach

--- apps/links/src/app/page.tsx
+++ apps/links/src/app/page.tsx
@@ -1,33 +1,14 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useDevice } from "./hooks/useDevice";
 
 export default function Home() {
-  const device = useDevice();
-
-  const links = {
-    instagram:
-      device === "desktop"
-        ? "https://www.instagram.com/rolamusic.app/"
-        : "instagram://user?username=rolamusic.app",
-    youtube:
-      device === "desktop"
-        ? "https://www.youtube.com/@rolamusicapp"
-        : "youtube.com/@rolamusicapp",
-    tiktok:
-      device === "desktop"
-        ? "https://www.tiktok.com/@rolamusicapp"
-        : "tiktok://user?username=rolamusicapp",
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="text-center space-y-4 pb-12">
         <Image
-          src={"/assets/img/icon.svg"}
+          className="m-auto"
+          src={"/assets/img/logo-rola-vertical-blanco.svg"}
           width={120}
           height={120}
           alt="Logo"
@@ -37,31 +18,31 @@ export default function Home() {
       <div className="flex flex-col gap-4 items-center">
         <Link
           className="w-60 text-center bg-white text-black py-2 px-8 rounded-full hover:bg-brand"
-          href="https://rolamusic.app/"
-          target="_blank"
-        >
-          ROLA
-        </Link>
-        <Link
-          className="w-60 text-center bg-white text-black py-2 px-8 rounded-full hover:bg-brand"
-          href={links.youtube}
+          href="https://urlgeni.us/youtube/channel/rola-youtube"
           target="_blank"
         >
           YouTube
         </Link>
         <Link
           className="w-60 text-center bg-white text-black py-2 px-8 rounded-full hover:bg-brand"
-          href={links.instagram}
+          href="https://urlgeni.us/tiktok/rola-tiktok"
           target="_blank"
         >
           Instagram
         </Link>
         <Link
           className="w-60 text-center bg-white text-black py-2 px-8 rounded-full hover:bg-brand"
-          href={links.tiktok}
+          href="https://rolamusic.app"
           target="_blank"
         >
           TikTok
+        </Link>
+        <Link
+          className="w-60 text-center bg-white text-black py-2 px-8 rounded-full hover:bg-brand"
+          href="https://urlgeni.us/instagram/rola-instagram"
+          target="_blank"
+        >
+          Web
         </Link>
       </div>
     </main>
