e7dc49987e86ccadd83fff9f85b67e0b27f4c96a
Edy Espinal <edyespinal@gmail.com>
2024-12-29 21:53:41 -0600

    @artists: add error page and update GlobalError page

--- apps/artists/src/app/error.tsx
+++ apps/artists/src/app/error.tsx
@@ -5,22 +5,28 @@ import { Header } from "@components/Header/Header";
 import { Button, Container, Text, Title } from "@rola/ui/components";
 import Link from "next/link";
 
-function GlobalError({ error }: { error: Error }) {
+function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
     <Container size="xl" className="grid h-svh grid-rows-[auto_1fr_auto]">
       <Header />
       <Container size="sm" className="grid place-items-center text-center">
         <div>
-          <Title>404</Title>
-          <Text className="pb-4">
+          <Title className="text-brand pb-4">:(</Title>
+
+          <Text className="">
             Lo sentimos, algo ha salido mal. Por favor, intenta de nuevo.
           </Text>
+          <Text className="pb-8">{error.message}</Text>
 
-          <Text>{error.message}</Text>
+          <Button onClick={() => reset()}>Intentar de nuevo</Button>
 
-          <Link href="/">
-            <Button>Volver al inicio</Button>
-          </Link>
+          <Text className="pt-24">Contacta con nuestro equipo de soporte</Text>
+          <a
+            href="mailto:soporte@rolamusic.app"
+            className="text-brand hover:underline"
+          >
+            soporte@rolamusic.app
+          </a>
         </div>
       </Container>
       <Footer />
@@ -28,4 +34,4 @@ function GlobalError({ error }: { error: Error }) {
   );
 }
 
-export default GlobalError;
+export default Error;
