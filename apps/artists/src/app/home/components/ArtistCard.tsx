26f168467b7e467d1fc11fe2ecaddc0a63f9c638
Edy Espinal <edyespinal@gmail.com>
2024-12-30 23:13:04 -0600

    @artists: update home page and components

--- apps/artists/src/app/home/components/ArtistCard.tsx
+++ apps/artists/src/app/home/components/ArtistCard.tsx
@@ -10,9 +10,9 @@ import {
 } from "@rola/ui/components";
 import { Artist } from "@rola/services/schemas";
 import { cn } from "@rola/tailwind-config/utils";
-import { useAtom } from "jotai";
-import { currentArtist } from "../../../store";
 import { useRouter } from "next/navigation";
+import { currentArtist } from "../../../store";
+import { useAtom } from "jotai";
 
 function ArtistCard({ artist }: { artist: Artist }) {
   const router = useRouter();
@@ -27,13 +27,16 @@ function ArtistCard({ artist }: { artist: Artist }) {
     <Container
       className={cn(
         "relative grid size-72 place-items-center gap-4 rounded-xl border p-4",
-        "hover:bg-background/50 hover:border-brand hover:cursor-pointer",
+        "hover:bg-background/50 hover:cursor-pointer",
         artist.active
-          ? "border-gray bg-transparent"
-          : "border-red-900 opacity-50"
+          ? "border-gray hover:border-brand bg-transparent"
+          : "border-destructive bg-background-dark hover:border-gray"
       )}
       onClick={handleClick}
     >
+      <span className="bg-destructive absolute -top-2 left-4 w-32 rounded-br-xl rounded-tl-xl px-4 text-sm">
+        No activo
+      </span>
       <Icon name="square-pen" className="text-brand absolute right-4 top-4" />
       <Avatar className="size-24">
         <AvatarImage src={artist.profileURL} alt={artist.name} />
