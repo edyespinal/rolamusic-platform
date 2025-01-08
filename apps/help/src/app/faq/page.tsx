23e1c5e6360016ad255fc46352cf5baeed436a9f
Edy Espinal <edyespinal@gmail.com>
2025-01-08 09:09:35 +0100

    @help: add back button to pages

--- apps/help/src/app/faq/page.tsx
+++ apps/help/src/app/faq/page.tsx
@@ -1,58 +1,7 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-  Container,
-  Text,
-  Title,
-  Underline,
-} from "@rola/ui/components";
-import { artistFaq, userFaq } from "./faq";
+import { FAQPageUI } from "./ui";
 
 function FAQPage() {
-  return (
-    <Container className="pt-12 pb-24">
-      <div className="pb-12">
-        <Title order={3} className="uppercase">
-          Preguntas Frecuentes
-        </Title>
-        <Underline />
-      </div>
-
-      <Title order={4} align="left" className="uppercase pb-4 text-brand">
-        Artistas
-      </Title>
-      <Accordion type="single" collapsible>
-        {artistFaq.map(({ question, answer }) => (
-          <AccordionItem value={question} key={question} className="mb-8">
-            <AccordionTrigger className="font-semibold">
-              <Text className="font-semibold">{question}</Text>
-            </AccordionTrigger>
-            <AccordionContent>
-              <Text className="pb-4">{answer}</Text>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
-
-      <Title order={4} align="left" className="uppercase pb-4 text-brand">
-        Usuarios
-      </Title>
-      <Accordion type="single" collapsible>
-        {userFaq.map(({ question, answer }) => (
-          <AccordionItem value={question} key={question} className="mb-8">
-            <AccordionTrigger className="font-semibold">
-              <Text className="font-semibold">{question}</Text>
-            </AccordionTrigger>
-            <AccordionContent className="pb-4">
-              <Text className="pb-4">{answer}</Text>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </Container>
-  );
+  return <FAQPageUI />;
 }
 
 export default FAQPage;
--- apps/help/src/components/Header/Header.tsx
+++ apps/help/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <Container as="header" size="xl" className="px-4 py-4 lg:px-0">
+    <Container as="header" size="lg" className="px-4 py-4 lg:px-0">
       <Link href="/">
         <Logo variant="horizontal" size="xs" />
       </Link>
