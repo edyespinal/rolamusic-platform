c7ba76150821650654a9e10b89b8ea76c45d8c1b
Edy Espinal <edyespinal@gmail.com>
2025-01-26 15:36:59 +0100

    @ui: Update Separator component

--- packages/ui/src/components/ui/Separator/Separator.tsx
+++ packages/ui/src/components/ui/Separator/Separator.tsx
@@ -17,7 +17,7 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-gray-light shrink-0",
+        "bg-gray-dark shrink-0",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className
       )}
