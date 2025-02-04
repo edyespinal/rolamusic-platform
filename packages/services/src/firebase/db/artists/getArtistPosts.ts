09e4b5376bdd4dfc62e98327d15fff4110f6ceca
Edy Espinal <edyespinal@gmail.com>
2025-02-04 11:52:10 +0100

    @services: return artists posts sorted by date

--- packages/services/src/firebase/db/artists/getArtistPosts.ts
+++ packages/services/src/firebase/db/artists/getArtistPosts.ts
@@ -17,14 +17,27 @@ async function getArtistPosts(artistId: string) {
       };
     }
 
-    return {
-      success: true,
-      data: postDocs.docs.map((doc) => {
+    const posts = postDocs.docs
+      .map((doc) => {
         return {
           ...doc.data(),
           id: doc.id,
         };
-      }),
+      })
+      .sort((a, b) => {
+        if (typeof a.date === "object" && typeof b.date === "object") {
+          const aDate = a.date as { seconds: number };
+          const bDate = b.date as { seconds: number };
+
+          return bDate.seconds - aDate.seconds;
+        }
+
+        return 0;
+      });
+
+    return {
+      success: true,
+      data: posts,
     };
   } catch (error) {
     const e = error as FirebaseError;
